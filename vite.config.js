4f6a50c38d5f68545f6c4c2084ca092e619d6561
Mikkel Holm Jensen <mik@danak.dk>
2024-03-04 13:31:23 +0100

    github pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/i18n-editor/",
   plugins: [
     vue(),
     Pages(),
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: '/'
+  publicPath: '/i18n-editor/',
+
 }
